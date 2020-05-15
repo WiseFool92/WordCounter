d158f5d7d2c6d59155ea3e214ee0ab0bd8c8819b
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-15 13:44:06 -0700

    meaningful fail for ConfirmSentence

--- WordCounter.Tests/ModelTests/WordCounter.Tests.cs
+++ WordCounter.Tests/ModelTests/WordCounter.Tests.cs
@@ -48,18 +48,24 @@ namespace WordCounter.Tests
     public void ConfirmWord_WordIsEqual_False()
     {
       // Arrange
-      string freshWord = "1337";
+      string freshWord = "1337Sauce";
       RepeatCounter testCheck = new RepeatCounter(freshWord, "confirm");
       // Act
       bool result = testCheck.WordConfirmation();
       // Assert
-      Assert.IsFalse(false, "1337"); 
+      Assert.IsFalse(false, "1337Sauce"); 
     }
 
     [TestMethod]
     public void ConfirmSentence_SentenceIsEqual_True()
     {
-      
+      // Arrange
+      string freshSentence = "Oil slicks the road";
+      RepeatCounter testCheck = new RepeatCounter("confirm", freshSentence);
+      // Act
+      bool result = testCheck.SentenceConfirmation();
+      // Assert
+      Assert.IsTrue(false, "slick road"); 
     }
   }
 }
