fbe895637bf1918bb450fd683988bc050c9f038f
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-15 16:33:38 -0700

    colorful console

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -16,15 +16,21 @@ namespace WordCounter
       bool play = true;
       while(play == true)
       {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         Console.WriteLine("Welcome to Word Counter!");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         Console.WriteLine("We will be counting the number of times your chosen word appears in your chosen sentence.");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         Enter:
 
         GetWordCount();
-      
-        Console.WriteLine("Shall we do another?\n\n[Y/N]");
-        string response = (Console.ReadLine().ToLower());
 
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine("Shall we do another?\n\n[Y/N]");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        string response = (Console.ReadLine().ToLower());
         if (response == "y")
         {
           goto Enter;
@@ -37,20 +43,32 @@ namespace WordCounter
     }
     public static void GetWordCount()
     {
+      Console.ForegroundColor = ConsoleColor.DarkBlue;
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       Console.WriteLine("Enter your word.");
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       string wordInput = Console.ReadLine();
+      Console.ForegroundColor = ConsoleColor.DarkGreen;
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       Console.WriteLine("Enter your sentence.");
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       string sentenceInput = Console.ReadLine();
 
       while (sentenceInput == "")
       {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         Console.WriteLine("Sorry please input a sentence to compare.");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         sentenceInput = Console.ReadLine();
       }
 
       RepeatCounter userInput = new RepeatCounter (wordInput, sentenceInput);
       int count = userInput.WordCount();
+      Console.ForegroundColor = ConsoleColor.DarkBlue;
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       Console.WriteLine("Your word was " + wordInput + " and was used " + count + " times in the provided sentence.");
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
   }
 }
