33dcc78ff7f32e65bf7dc9f74d5a3b846fd6ac1f
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-15 15:41:40 -0700

    uncomment UI & create the GetWordCount() method

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -8,37 +8,49 @@ namespace WordCounter
   {
     public static void Main()
     {
-      // Start();
+      Start();
     }
 
-    // public void Start()
-    // {
-    //   bool play = true;
-    //   while(play == true)
-    //   {
-    //     Console.WriteLine("Welcome to Word Counter!");
-    //     Console.WriteLine("We will be counting the number of of times your chosen word appears in your chosen sentence.");
-    //     Enter:
+    public static void Start()
+    {
+      bool play = true;
+      while(play == true)
+      {
+        Console.WriteLine("Welcome to Word Counter!");
+        Console.WriteLine("We will be counting the number of of times your chosen word appears in your chosen sentence.");
+        Enter:
 
+        GetWordCount();
+      
+        Console.WriteLine("Shall we do another?\n\n[Y/N]");
+        string response = (Console.ReadLine().ToLower());
+        
+        if (response == "y")
+        {
+          goto Enter;
+        }
+        else
+        {
+          play = false;
+        }
+      }
+    }
+    public static void GetWordCount()
+    {
+      Console.WriteLine("Enter your word.");
+      string wordInput = Console.ReadLine();
+      Console.WriteLine("Enter your sentence.");
+      string sentenceInput = Console.ReadLine();
 
-    //     Console.WriteLine("Enter your word.");
-    //     string wordInput = Console.ReadLine();
-    //     Console.WriteLine("Enter your sentence.");
-    //     string sentenceInput = Console.ReadLine();
-    //     GetWordCount(input);
-    //     DisplayWordCount();
+      while (sentenceInput == "")
+      {
+        Console.WriteLine("Sorry please input a sentence to compare.");
+        sentenceInput = Console.ReadLine();
+      }
 
-    //     Console.WriteLine("Shall we do another?\n\n[Y/N]");
-    //     string response = (Console.ReadLine().ToLower());
-    //     if (response == "y")
-    //     {
-    //       goto Enter;
-    //     }
-    //     else
-    //     {
-    //       play = false;
-    //     }
-    //   }
-    // }
+      RepeatCounter userInput = new RepeatCounter (wordInput, sentenceInput);
+      int count = userInput.WordCount();
+      Console.WriteLine("Your word was " + wordInput + " and was used " + count + " times in the provided sentence.");
+    }
   }
 }
