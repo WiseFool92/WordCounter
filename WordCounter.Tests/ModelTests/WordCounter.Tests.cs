83f2ede419e62fffbcecccbbac9072cdd18b3132
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-15 08:19:08 -0700

    initial

---
+++ WordCounter.Tests/ModelTests/WordCounter.Tests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Model;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class ItemTests : IDisposable
+  {
+    public void Dispose()
+    {
+
+    }
+    [TestMethod]
+  }
+}
---
+++ WordCounter/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using WordCounter.Model;
+
+namespace WordCounter
+{
+  class Program
+  {
+    public static void Main()
+    {
+      
+    }
+  }
+}
