7335a49a01a7e301e695155543c153a2b38a9113
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-15 09:02:09 -0700

    UI framework

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -8,7 +8,37 @@ namespace WordCounter
   {
     public static void Main()
     {
-      
+      Start();
+    }
+    
+    public void Start()
+    {
+      bool play = true;
+      while(play == true)
+      {
+        Console.WriteLine("Welcome to Word Counter!");
+        Console.WriteLine("We will be counting the number of of times your chosen word appears in your chosen sentence.");
+        Enter:
+
+
+        Console.WriteLine("Enter your word.");
+        string wordInput = Console.ReadLine();
+        Console.WriteLine("Enter your sentence.");
+        string sentenceInput = Console.ReadLine();
+        GetWordCount(input);
+        DisplayWordCount();
+
+        Console.WriteLine("Shall we do another?\n\n[Y/N]");
+        string response = (Console.ReadLine().ToLower());
+        if (response == "y")
+        {
+          goto Enter;
+        }
+        else
+        {
+          play = false;
+        }
+      }
     }
   }
 }
